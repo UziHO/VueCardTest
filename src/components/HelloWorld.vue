8d12798cf4ce0c544d9f40abebd4cff83dfbd6e4
Uziel HO <uziel@sfi.digital>
2024-04-22 11:00:33 -0600

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,25 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>Uzi es genial</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      A rapear
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="form-group email mb-xs-4 mb-md-5">
+        <label for="username">{{ emailOrCID }}</label>
+        <input
+          data-flow="signup"
+          type="text"
+          id="username"
+          v-model="userIdentifier"
+          @input="handleInput"
+          :class="{'input-error': errorExists}"
+          :maxlength="idMaxlength"
+          v-abn-input="email"
+          inputmode="email"
+          autocomplete="username"
+        />
+      </div>
+   
   </div>
 </template>
 
@@ -35,6 +28,19 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      userIdentifier: '',
+      errorExists: false,
+      idMaxlength: 100, // Definir el valor máximo permitido
+      emailOrCID: 'Email o CID' // Definir el texto de i18n aquí
+    };
+  },
+  methods: {
+    handleInput() {
+      // Aquí puedes realizar cualquier validación o manipulación necesaria con userIdentifier
+    }
   }
 }
 </script>
